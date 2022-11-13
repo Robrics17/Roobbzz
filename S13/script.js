bb886dbd7726572acd467e41d769a8c60f5f7f12
unknown <you@example.com>
2022-11-13 21:33:01 +0800

    commit

---
+++ S13/script.js
@@ -0,0 +1,28 @@
+console.Log ("Connected");
+//JS Operators
+//symbols used to manipulate values/operands
+//used to perform specific mathematical and logical computation in operands.
+
+//1. Assignment Operators
+//represents the equal symbols (=)
+
+let a = 14;
+a = 18;
+
+let b = a;
+console.Log (b);
+// in this sample, using let can reassign the values of a variable.
+
+//2. Arithmetic Opearators - mathematical operations on a nuerical values/operands and returns a single numerical value.
+
+console.Log (20+30); //50 Primitive Data Type
+console.Log ("20"+"30"); //String
+//String Concatenation or just the plain concatenation
+//it links or combine strings together
+
+//Substraction/difference (-)
+console.Log (20-10); //10 - this was  declared as a number
+
+console.Log ("50"-"10"); //40
+//Type Coercion - automatic/implicit conversion of values from one data type to another.
+
